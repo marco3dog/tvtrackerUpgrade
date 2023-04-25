721ba723c6ddb7370bc2a21c0bd9f7e3da235009
marco3dog <62681327+marco3dog@users.noreply.github.com>
2023-04-25 07:28:10 -0700

    added src

---
+++ TVTracker/src/com/cognixia/jump/exceptions/PasswordNotFoundException.java
@@ -0,0 +1,9 @@
+package com.cognixia.jump.exceptions;
+
+public class PasswordNotFoundException extends Exception {
+
+	public PasswordNotFoundException(String message) {
+		super(message);
+	}
+
+}
