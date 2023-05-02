a374d02f41043e4e164ce6f09cc0f1721269fc09
Cliff Freeman <oathkeepreoblivion@gmail.com>
2023-05-02 11:50:42 -0500

    added more spacing and stuff

--- src/com/tvshowtracker/dao/TVTrackerDaoSql.java
+++ src/com/tvshowtracker/dao/TVTrackerDaoSql.java
@@ -111,7 +111,7 @@ public class TVTrackerDaoSql {
 					+ ", 0)");
 
 			if (updated != 0)
-				System.out.println(ConsoleColors.GREEN + "Show successfully added to list." + ConsoleColors.RESET);
+				System.out.println(ConsoleColors.GREEN + "Show successfully added to list.\n" + ConsoleColors.RESET);
 			return true;
 
 		} catch (SQLException e) {
@@ -405,7 +405,7 @@ public class TVTrackerDaoSql {
 			int updated = stmt.executeUpdate("UPDATE user_shows SET episodes = " + episodesWatched + " WHERE showid = " + showId + " AND userid = " + user.getId());
 
 			if (updated != 0)
-				System.out.println(ConsoleColors.GREEN + "List entry successfully updated." + ConsoleColors.RESET);
+				System.out.println(ConsoleColors.GREEN + "List entry successfully updated.\n" + ConsoleColors.RESET);
 			return true;
 
 		} catch (SQLException e) {
